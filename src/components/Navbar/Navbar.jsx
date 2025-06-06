f14f6d63ff9482f83cd1cf151302e7b360ec1983
filipesantanadev <filipe.santana@al.infnet.edu.br>
2025-06-06 01:05:02 -0300

    feat: add RHF with Zod and error for empty/blank title

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,58 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import logo from "../../images/logoBN.png";
-import { Button, ImageLogo, InputSpace, Nav } from "./NavbarStyled";
+import { Button, ErrorSpan, ImageLogo, InputSpace, Nav } from "./NavbarStyled";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const searchSchema = z.object({
+  title: z
+    .string()
+    .nonempty({ message: "A pesquisa não pode estar vazia" })
+    .refine((value) => !/^\s*$/.test(value), {
+      message: "A pesquisa não pode ter apenas espaços",
+    }),
+});
 
 export function Navbar() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(searchSchema),
+  });
+  const navigate = useNavigate();
+
+  function onSearch(data) {
+    const { title } = data;
+    navigate(`/search/${title}`);
+    reset();
+  }
   return (
     <>
       <Nav>
-        <InputSpace>
-          <i className="bi bi-search"></i>
-          <input type="text" placeholder="Pesquise por um título" />
-        </InputSpace>
+        <form onSubmit={handleSubmit(onSearch)}>
+          <InputSpace>
+            <button type="submit">
+              <i className="bi bi-search"></i>
+            </button>
+            <input
+              {...register("title")}
+              type="text"
+              placeholder="Pesquise por um título"
+            />
+          </InputSpace>
+        </form>
 
-        <ImageLogo src={logo} alt="Logo do Breaking News" />
+        <Link to="/">
+          <ImageLogo src={logo} alt="Logo do Breaking News" />
+        </Link>
 
         <Button>Entrar</Button>
       </Nav>
+      {errors.title && <ErrorSpan>{errors.title.message}</ErrorSpan>}
       <Outlet />
     </>
   );
